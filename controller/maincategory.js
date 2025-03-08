9f25ead31fdaef9288ed8ffc9770eb2a5e32b8c0
Narjisha K <144308345+NarjishaK@users.noreply.github.com>
2025-03-09 02:49:02 +0530

    crud main category

---
+++ controller/maincategory.js
@@ -0,0 +1,81 @@
+const MainCategory = require("../models/maincategory");
+const asyncHandler = require("express-async-handler");
+
+//create main category
+exports.create = asyncHandler(async (req, res) => {
+    const { name } = req.body;
+    const image = req.file.filename;
+    const maincategory = await MainCategory.create({ name, image });
+    res.status(200).json(maincategory);
+});
+
+//get all main category
+exports.getAll = asyncHandler(async (req, res) => {
+    const maincategory = await MainCategory.find();
+    res.status(200).json(maincategory);
+});
+
+//get main category by id
+exports.get = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const maincategory = await MainCategory.findById(id);
+        if (!maincategory) {
+            return res.status(404).json({ message: "Main category not found" });
+        }
+        res.status(200).json(maincategory);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+
+//update main category by id
+exports.update = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const image = req.file ? req.file.filename : null;
+
+    try {
+        const maincategory = await MainCategory.findById(id);
+        if (!maincategory) {
+            return res.status(404).json({ message: "Main category not found" });
+        }
+        maincategory.name = name || maincategory.name;
+        maincategory.image = image || maincategory.image;
+        await maincategory.save();
+        res.status(200).json(maincategory);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+//delete main category by id
+exports.delete = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const maincategory = await MainCategory.findByIdAndDelete(id);
+        if (!maincategory) {
+            return res.status(404).json({ message: "Main category not found" });
+        }
+        res.status(200).json({ message: "Main category deleted successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+
+
+//delete all main category
+exports.deleteAll = asyncHandler(async (req, res) => {
+    try {
+        await MainCategory.deleteMany({});
+        res.status(200).json({ message: "All main category deleted successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
---
+++ routes/mainctaegory.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const Controller = require("../controller/maincategory");
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage: storage });
+
+router.post("/", upload.single("image"), Controller.create);
+router.get("/", Controller.getAll);
+router.get("/:id", Controller.get);
+router.put("/:id", upload.single("image"), Controller.update);
+router.delete("/:id", Controller.delete);
+router.delete("/", Controller.deleteAll);
+module.exports = router;
